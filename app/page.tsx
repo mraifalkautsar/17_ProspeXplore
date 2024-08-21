d17c02be7590e1652452528ca2c04e33c36da77a
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-08-21 13:42:19 +0700

    feat: initialized auth page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <header className='header'>
-          <a className='logo'>PROSPEXPLORE</a>
-
-          <nav className='navbar'>
-            <a href="#profile">Profile</a>
-            <a href="#Referral">Log In</a>
-          </nav>
-        </header>
+        <a className='logo'>PROSPEXPLORE</a>
+        <nav className='navbar'>
+          <a href="#profile">Profile</a>
+          <a href="#Referral">Log In</a>
+        </nav>
+      </header>
+      <body>
+        <Link href="/authentication">Auth</Link>
+      </body>
+      
     </main>
   );
 }
+

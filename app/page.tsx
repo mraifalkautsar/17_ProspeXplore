740515ab2edba6f26e18fd8787007864f89ebebd
PeterWongsoredjo <19623284@std.stei.itb.ac.id>
2024-08-21 23:19:35 +0800

    feat: Initialized Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,32 @@ export default function Home() {
         </nav>
       </header>
       <body>
-        <Link href="/authentication">Auth</Link>
+        <a href="/authentication">Auth</a>
       </body>
-      
+      <div className="tagline-container">
+        <div className="tagline-head">
+          <h1>PROSPEXPLORE</h1>
+        </div>
+        <div className="tagline">
+          <p>Discover Your Future, Trace Your Success</p>
+        </div>
+      </div>
+      <div className="kesan-container">
+        <div className="foto-alumni">
+          <p>[Foto]</p>
+        </div>
+        <div className="kesan-pesan">
+          <div className="kesan-alumni">
+            <p>[Kesan Alumni yang Sudah Pakai Web Ini]</p>
+          </div>
+          <div className="nama-alumni">
+            <p>[Nama Alumni]</p>
+          </div>
+          <div className="posisi-alumni">
+            <p>[Alumni Kampus dan Posisi Saat Ini]</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }

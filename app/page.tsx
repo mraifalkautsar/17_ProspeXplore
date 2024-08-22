13c7a9f9d452d0ff605a03981abda1a383b9d65b
Rafif Farras <rafiffarras@macbook-air.local>
2024-08-22 18:36:32 +0700

    fakultas

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,91 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <header className='header'>
-          <a className='logo'>PROSPEXPLORE</a>
+      <header className="header">
+        <a className="logo">PROSPEXPLORE</a>
 
-          <nav className='navbar'>
-            <a href="#profile">Profile</a>
-            <a href="#Referral">Log In</a>
-          </nav>
-        </header>
+        <nav className="navbar">
+          <a href="#profile">Profile</a>
+          <a href="#Referral">Log In</a>
+        </nav>
+      </header>
+      <h1 className="text-4xl mt-10 lg:text-6xl font-bold text-center text-[#055096] mb-8">
+        FAKULTAS
+      </h1>
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-6 gap-4 pt-20">
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="1.png" alt="Button 1" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="2.png" alt="Button 2" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="3.png" alt="Button 3" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="4.png" alt="Button 4" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="5.png" alt="Button 5" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="6.png" alt="Button 6" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="7.png" alt="Button 7" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="8.png" alt="Button 8" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="9.png" alt="Button 9" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="10.png" alt="Button 10" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="11.png" alt="Button 11" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center mb-10">
+          <button className="w-24 h-24 lg:w-28 lg:h-28 bg-gray-100 rounded-full flex justify-center items-center hover:bg-black hover:border-4 hover:border-[#055096] hover:scale-110 transition-transform duration-300 ease-in-out p-1 hover:shadow-[0_0_25px_rgba(255,255,255,0.9)]">
+            <img src="12.png" alt="Button 12" className="w-22 h-22 lg:w-26 lg:h-26 rounded-full hover:opacity-80" />
+          </button>
+        </div>
+
+      </div>
     </main>
   );
 }

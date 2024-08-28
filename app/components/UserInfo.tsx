0b59df3fe8e92dcaa4f43d6ad8796a69d5bf60e8
PeterWongsoredjo <19623284@std.stei.itb.ac.id>
2024-08-28 22:04:36 +0700

    feat: connected email

--- app/components/UserInfo.tsx
+++ app/components/UserInfo.tsx
@@ -3,6 +3,7 @@
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function UserInfo() {
   const { data: session } = useSession();
@@ -13,17 +14,39 @@ export default function UserInfo() {
     router.push("/authentication");
   };
 
+
   return (
-      <div className="w-full mx-auto items-center justify-center text-center pt-40">
-        <div className="font-poppins font-extrabold text-4xl inline-flex">
-          <h2 className="text-white">Hey,</h2><span className="bg-gradient-to-r from-white to-[#6B58B3] text-transparent bg-clip-text">&nbsp;{session?.user?.email}</span>
-        </div>
-        <p className="font-poppins text-white text-xl mt-4 font-semibold">What can we do for you?</p>
+      <div className="justify-center items-center">
+        <Image
+          src="/pfp.png"
+          alt="pfp"
+          width={180}
+          height={180}
+          className="mx-auto my-5"
+        />
+
+        <form className="mx-40 pb-16 px-32">
+          <p className="font-bold font-poppins text-white mb-2">Email Address</p>
+          <div className="mb-4 text-white">
+            <span className="block w-full px-3 py-3 border-2 rounded-full">&nbsp;{session?.user?.email}</span>          
+          </div>
+
+          <p className="font-bold font-poppins text-white mb-2">Password</p>
+          <div className="mb-4 text-white">
+            <span className="block w-full px-3 py-3 border-2 rounded-full">*****</span>
+          </div>
+        </form>
+
         <button 
             onClick={() => signOut({ callbackUrl: "/authentication" })}
             className="w-40 rounded-full bg-gradient-to-r from-[#CAAACF] to-[#6B58B3] text-white font-poppins font-extrabold m-8 py-3 px-4 shadow-lg">
                 Log Out
         </button>
+
+        <div className="flex fixed bottom-0">
+          <img src="Vector.png" alt="logo" className="h-8 my-6 ml-6 mr-2" />
+          <img src="Frame 13.png" alt="name" className="h-8 mt-6" />
+        </div>
       </div>
     );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,23 @@ import UserInfo from "../components/UserInfo";
 export default function Dashboard() {
   return(
     <main className="bg-gradient-to-b from-[#1A3594] to-[#6B58B3] min-h-screen">
+      <div className="header-container">
+        <div className="header">
+          <img 
+            src="/logo.png"
+            alt="ProspeXplore"
+            className="logo"
+          />
+          <div className="navbar">
+            <a href="/">Beranda</a>
+            <a href="#fakultas">Fakultas</a>
+            <a href="#tes-minat">Tes Minat</a>
+            <a href="/authentication">
+              <span className="sign-in-button">Sign In</span>
+            </a>
+          </div>
+        </div>
+      </div>
       <UserInfo/>
       
       <div className="flex absolute bottom-0">

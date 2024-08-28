bab49febefbbcc0d430d9e6aabfdfff7b1fd2160
PeterWongsoredjo <19623284@std.stei.itb.ac.id>
2024-08-29 00:28:35 +0700

    fix: trial-1 fixing error

--- app/models/user.js
+++ app/models/user.js
@@ -14,5 +14,5 @@ const userSchema = new Schema(
   },
 );
 
-const User = models.User || mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -2,7 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcryptjs';
 import { connectMongoDB } from "@/lib/mongodb";
-import User from '@/models/user';
+import User from '@/models/User';
 
 import GoogleProvider from "next-auth/providers/google";
 

8acb774d86abab92ef30eb514e2f9d7b2ff1de7b
PeterWongsoredjo <19623284@std.stei.itb.ac.id>
2024-08-28 23:52:07 +0700

    feat: blank space

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <header className='header'>
-          <a className='logo'>PROSPEXPLORE</a>
-
-          <nav className='navbar'>
-            <a href="#profile">Profile</a>
-            <a href="#Referral">Log In</a>
-          </nav>
-        </header>
     </main>
   );
 }
